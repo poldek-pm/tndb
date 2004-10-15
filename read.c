561200886b7857b915251acdbbcf0ae64a943361
Paweł Gajda <mis@pld-linux.org>
2004-10-15 19:18:05 +0000

    - fixed fmt

--- read.c
+++ read.c
@@ -648,8 +648,8 @@ int tndb_it_get_end(struct tndb_it *it)
     off = n_stream_tell(it->_db->st);
 
     if (off > (int)it->_off) {
-        n_die("tndb_it_get_end: current offset is %lu, expected %lu\n",
-              off, it->_off);
+        printf("tndb_it_get_end: current offset is %lu, expected %u\n", off,
+               it->_off);
         return 0;
     }
     
