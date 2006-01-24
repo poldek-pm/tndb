22c6fafcbf32b7ee467adab5cde0a3acb9176c83
Paweł Gajda <mis@pld-linux.org>
2006-01-24 20:37:52 +0000

    - s/@pld.org.pl/@k2.net.pl/

--- read.c
+++ read.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2002 Pawel A. Gajda <mis@pld.org.pl>
+  Copyright (C) 2002 Pawel A. Gajda <mis@k2.net.pl>
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU Library General Public License, version 2
